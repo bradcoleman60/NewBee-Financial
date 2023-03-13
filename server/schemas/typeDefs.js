7f9487cd7117c62fd968131d69adeab78684f4bd
Troy <tnj8510@gmail.com>
2023-03-13 03:31:34 -0700

    update typedef add company for user input

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,54 +1,72 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-    type User {
-        _id: ID!
-        username: String!
-        email: String!
-        savedCompanies: [Company]
-    }
-    
-    type Company {
-        _id: ID
-        cik: Int
-        name: String
-        ticker: String
-        exchange: String
-        revenue: String
-        revenue1: String
-        netIncome: String
-        cash: String
-        cashFlow: String
-        cashFlow1: String
-        eps: Float
-        eps1: Float
-        currentAsset: String
-        currentLiabilities: String
-        taxesPaid: String
-        taxesPaid1: String
-    }
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    savedCompanies: [Company]
+  }
 
-    type Auth {
-        token: ID!
-        user: User
-    }
+  type Company {
+    _id: ID
+    cik: Int
+    name: String
+    ticker: String
+    exchange: String
+    revenue: String
+    revenue1: String
+    netIncome: String
+    cash: String
+    cashFlow: String
+    cashFlow1: String
+    eps: Float
+    eps1: Float
+    currentAsset: String
+    currentLiabilities: String
+    taxesPaid: String
+    taxesPaid1: String
+  }
 
-    type Query {
-        users: [User]
-        user (_id: String): User
-        company: [Company]
-    }
+  type Auth {
+    token: ID!
+    user: User
+  }
 
-    type Mutation {
-        createUser(username: String!, email: String!, password: String!): Auth
-        login(username: String!, password: String!): Auth
-        
-        addCompany(userId: ID!, cik: Int!): User
-        removeCompany(userId: ID!, cik: Int!): User
-        
-        updateUser(cik: Int!): User
-        deleteUser(_id: String!) : User
-    }
-`
+  input CompanyInput {
+    cik: Int
+    name: String
+    ticker: String
+    exchange: String
+    revenue: String
+    revenue1: String
+    netIncome: String
+    cash: String
+    cashFlow: String
+    cashFlow1: String
+    eps: Float
+    eps1: Float
+    currentAsset: String
+    currentLiabilities: String
+    taxesPaid: String
+    taxesPaid1: String
+  }
 
-module.exports = typeDefs
+  type Query {
+    users: [User]
+    user(_id: ID!): User
+    companies: [Company]
+  }
+
+  type Mutation {
+    createUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    updateUser(_id: ID!, username: String, email: String, password: String): User
+    deleteUser(_id: ID!): User
+    addCompany(company: CompanyInput!): User
+    removeCompany(cik: Int!): User
+  }
+  
+`;
+
+module.exports = typeDefs;
