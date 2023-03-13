a6e03ee0523b35e935643ec4a98d9e89f9ed97c5
Troy <tnj8510@gmail.com>
2023-03-13 04:06:47 -0700

    adding save companies/remove companies mutations client side

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,15 +1,6 @@
 import { gql } from '@apollo/client';
 
-
-export const LOGIN_MUTATION = gql`
-  mutation LoginMutation($username: String!, $password: String!) {
-    login(username: $username, password: $password) {
-      token
-    }
-  }
-`;
-
-export const ADD_USER = gql`
+export const CREATE_USER = gql`
   mutation addUser(
     $username: String!
     $email: String!
@@ -23,6 +14,79 @@ export const ADD_USER = gql`
       token
       user {
         _id
+      }
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation LoginMutation($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      token
+    }
+  }
+`;
+
+const UPDATE_USER = gql`
+  mutation UpdateUser($_id: ID!, $username: String, $email: String, $password: String) {
+    updateUser(_id: $_id, username: $username, email: $email, password: $password) {
+      _id
+      username
+      email
+    }
+  }
+`;
+
+// Mutation to delete a user
+export const DELETE_USER = gql`
+  mutation deleteUser($_id: ID!) {
+    deleteUser(_id: $_id) {
+      _id
+      username
+      email
+    }
+  }
+`;
+
+const SAVE_COMPANY = gql`
+  mutation SaveCompany($company: CompanyInput!) {
+    saveCompany(company: $company) {
+      _id
+      savedCompanies {
+        _id
+        cik
+        name
+        ticker
+        exchange
+        revenue
+        revenue1
+        netIncome
+        cash
+        cashFlow
+        cashFlow1
+        eps
+        eps1
+        currentAsset
+        currentLiabilities
+        taxesPaid
+        taxesPaid1
+      }
+    }
+  }
+`;
+
+// Mutation to remove a company
+export const REMOVE_COMPANY = gql`
+  mutation removeCompany($cik: Int!) {
+    removeCompany(cik: $cik) {
+      _id
+      username
+      email
+      savedCompanies {
+        _id
+        cik
+        name
+        ticker
       }
     }
   }
