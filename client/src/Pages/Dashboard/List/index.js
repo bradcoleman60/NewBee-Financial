79d554fdbb311619a0b907855342f9e33ccca0ec
Troy <tnj8510@gmail.com>
2023-03-15 18:10:55 -0700

    add buttons

--- client/src/Pages/Dashboard/List/index.js
+++ client/src/Pages/Dashboard/List/index.js
@@ -1,13 +1,27 @@
-// import React from 'react'
+import React from "react";
 // import Company from './Company'
-// import { useCompanyContext } from '../../../utils/companyContext'
-// import { idbPromise } from '../../../utils/helper'
+import { useCompanyContext } from "../../../utils/companyContext";
+import { idbPromise } from "../../../utils/helper";
 
-// export default function index() {
+export default function List() {
 
+  const [testData, setTestData] = React.useState([]);
 
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const data = await idbPromise("get");
+      setTestData(data);
+    };
 
-//   return (
-//     <Company/>
-//   )
-// }
+    fetchData();
+  }, []);
+
+  console.log(testData.map(el => el.name));
+
+  return (    <div>
+    <h2>List of Names:</h2>
+      {testData.map((el) => (
+        <button key={el.id}>{el.name}</button>
+      ))}
+  </div>)
+}
