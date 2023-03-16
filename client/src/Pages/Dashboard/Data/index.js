4e651e42996d393b7d8a93b3f2f46615fe737b09
Troy <tnj8510@gmail.com>
2023-03-15 21:24:48 -0700

    adds idb data to table

--- client/src/Pages/Dashboard/Data/index.js
+++ client/src/Pages/Dashboard/Data/index.js
@@ -1,75 +1,52 @@
-import React from "react";
-import Company from "./Company";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import './style.css'
+import { idbPromise } from "../../../utils/helper";
+import "./style.css";
 
 export default function Data(props) {
-    const companies = [{
-        name: 'Amazon',
-        revenue: 111111,
-        cashFlow: 22222,
-        netEarnings: 333333,
-        cash: 4444444
+  const [companies, setCompanies] = useState([]);
 
-    },
-    {
-        name: 'Tesla',
-        revenue: 111111,
-        cashFlow: 22222,
-        netEarnings: 333333,
-        cash: 4444444
+  useEffect(() => {
+    const fetchData = async () => {
+      const idbCompanies = await idbPromise("get");
+      setCompanies(idbCompanies);
+    };
 
-    },
-    {
-        name: 'Meta',
-        revenue: 111111,
-        cashFlow: 22222,
-        netEarnings: 333333,
-        cash: 4444444
+    fetchData();
+  }, []);
 
-    },
-    {
-        name: 'Apple',
-        revenue: 111111,
-        cashFlow: 22222,
-        netEarnings: 333333,
-        cash: 4444444
+  const selectedProps = [
+    { label: "Revenue", property: "revenue" },
+    { label: "Cash Flow", property: "cashFlow" },
+    { label: "Earnings", property: "earnings" },
+    { label: "EPS", property: "eps" },
+    { label: "Cash", property: "cash" }
+  ];
 
-    },
-]
-
-  const companyColumns = companies.map((company, index) => (
-    <Company
-      key={index}
-      name={company.name}
-      revenue={company.revenue}
-      cashFlow={company.cashFlow}
-      netEarnings={company.netEarnings}
-      cash={company.cash}
-    />
-  ));
-  function handleClick (){
-    window.location.replace('./learn')
-
+  function handleClick() {
+    window.location.replace("./learn")
   }
 
   return (
-    <section>
-      <h3>Financial Health</h3>
-      <table className="financial-table">
-        <thead>
-          <tr>
-            <th onClick = {handleClick}>Company</th>
-            <th onClick = {handleClick}> Revenue </th>
-            <th onClick = {handleClick}>Cash Flow</th>
-            <th onClick = {handleClick}>Net Earnings</th>
-            <th onClick = {handleClick}>Cash</th>
+    <table className="financial-table">
+      <thead>
+        <tr>
+          <th></th>
+          {companies.map(company => (
+            <th key={company.name}>{company.name}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {selectedProps.map(prop => (
+          <tr key={prop.label}>
+            <td onClick={handleClick}>{prop.label}</td>
+            {companies.map(company => (
+              <td key={company.name}>{company[prop.property] || "N/A"}</td>
+            ))}
           </tr>
-        </thead>
-        <tbody>
-          {companyColumns}
-        </tbody>
-      </table>
-    </section>
+        ))}
+      </tbody>
+    </table>
   );
 }
